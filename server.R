330b543e5b2cdffc9e0b2b33e6fe743d0c99952a
Nadimchouaib <ndm.bouchaib@gmail.com>
2025-04-06 09:54:27 +0100

    Extend downloading to all files and update "Clone" button UX
    Extend the cloning and downloading functionality to support all file types not only files from study_objects.
    
    Improve the "Clone" button by adding an icon, enhancing visual consistency and the overall user experience.

--- server.R
+++ server.R
@@ -323,15 +323,15 @@ server <- function(input, output, session) {
     selected_data_path(path)
     relative_path(str_remove(path, "^.*/Users/"))
 
-    if (grepl("study_objects", path)) {
       showModal(
         modalDialog(
           div(
             paste0("Do you want to clone or download ", relative_path(), "?"),
-            style = "font-size: 25px; margin-bottom: 20px; text-align: center;"
-          ),
+            style = "font-size: 25px; margin-bottom: 20px; text-align: center; word-wrap: break-word; overflow-wrap: break-word; max-width: 100%;"
+          )
+          ,
           footer = tagList(
-            actionButton("confirm_clone_super_user", "Clone", class = "btn-primary"),
+            actionButton("confirm_clone_super_user", label = tagList(icon("copy"), "Clone"),, class = "btn-primary"),
             downloadButton("download_super_user", "Download", class = "btn-primary"),
             modalButton("Cancel")
           ),
@@ -339,14 +339,6 @@ server <- function(input, output, session) {
           easyClose = TRUE
         )
       )
-    } else {
-      shinyWidgets::ask_confirmation(
-        inputId = "confirm_clone_super_user",
-        type = "question",
-        title = "Do you want to clone the data to your folder?",
-        btn_colors = c("grey", "#009ADB")
-      )
-    }
   })
 
   # Handle download of selected user data
@@ -354,8 +346,8 @@ server <- function(input, output, session) {
     filename = function() {
       rp <- relative_path()
       safe_name <- gsub("/", "_", rp)
-      paste0(tools::file_path_sans_ext(safe_name), ".json")
-    },
+      paste(safe_name)
+      },
     content = function(file) {
       sp <- selected_data_path()
       rp <- relative_path()
